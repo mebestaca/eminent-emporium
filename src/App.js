c6ed86dd6c60ef189c9751cf8558299b63817f00
Marc Edison Estaca <mebestaca@gmail.com>
2023-08-19 13:52:54 -0600

    moved navigation into its own folder

--- src/App.js
+++ src/App.js
@@ -1,16 +1,7 @@
-import Home from "./components/routes/home.component";
-import { Route, Routes, Outlet } from "react-router-dom";
+import Home from "./components/routes/home/home.component";
+import Navigation from "./components/routes/navigation/navigation.component";
+import { Route, Routes } from "react-router-dom";
 
-const Navigation = () => {
-  return (
-    <div>
-      <div>
-        <h1>Navigation Bar Placeholder</h1>
-      </div>
-      <Outlet/>
-    </div>
-  );
-}
 
 const Shop = () => {
   return (
