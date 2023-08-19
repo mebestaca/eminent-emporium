270b45216473f0c149ff864e33d13438a0c049e0
Marc Edison Estaca <mebestaca@gmail.com>
2023-08-19 13:19:51 -0600

    added navigation bar placeholder

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
 import Home from "./components/routes/home.component";
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, Outlet } from "react-router-dom";
+
+const Navigation = () => {
+  return (
+    <div>
+      <div>
+        <h1>Navigation Bar Placeholder</h1>
+      </div>
+      <Outlet/>
+    </div>
+  );
+}
+
+const Shop = () => {
+  return (
+    <div>
+      <h1>Shop Section</h1>
+    </div>
+  );
+}
 
 const App = () => {
   return (
     <Routes>
-      <Route path='/' element={ <Home/> } />
+      <Route path='/' element={ <Navigation/> } >
+        <Route index element={ <Home/> } />
+        <Route path='shop' element={ <Shop/> } />
+      </Route>
     </Routes>
   );
 }
