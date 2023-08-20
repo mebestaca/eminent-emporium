fcd5fb82539d747b0b2a1257b95e452f3ad66a9a
Marc Edison Estaca <mebestaca@gmail.com>
2023-08-20 12:12:13 -0600

    added firebase and sign in placeholder page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import Home from "./components/routes/home/home.component";
 import Navigation from "./components/routes/navigation/navigation.component";
+import SignIn from "./components/routes/sign-in/sign-in.component";
 import { Route, Routes } from "react-router-dom";
 
 
@@ -17,6 +18,7 @@ const App = () => {
       <Route path='/' element={ <Navigation/> } >
         <Route index element={ <Home/> } />
         <Route path='shop' element={ <Shop/> } />
+        <Route path='sign-in' element={ <SignIn/> } />
       </Route>
     </Routes>
   );
