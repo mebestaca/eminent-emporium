bb9c130c195412c890b13652493b22bb9c146235
Marc Edison Estaca <mebestaca@gmail.com>
2023-08-26 17:32:44 -0600

    added mock shop data and shop temporary page

--- src/App.js
+++ src/App.js
@@ -1,16 +1,8 @@
 import Home from "./components/routes/home/home.component";
 import Navigation from "./components/routes/navigation/navigation.component";
 import Authentication from "./components/routes/authentication/authentication.component";
+import Shop from "./components/shop/shop.component";
 import { Route, Routes } from "react-router-dom";
-
-
-const Shop = () => {
-  return (
-    <div>
-      <h1>Shop Section</h1>
-    </div>
-  );
-}
 
 const App = () => {
   return (
