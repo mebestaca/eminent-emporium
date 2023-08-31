f7c54265ab18de9cca1343824729b567e7aefc30
Marc Edison Estaca <mebestaca@gmail.com>
2023-08-31 12:22:01 -0600

    added cart style

--- src/components/cart-item/cart-item.component.jsx
+++ src/components/cart-item/cart-item.component.jsx
@@ -3,9 +3,12 @@ import './cart-item.style.scss';
 const CartItem = ({ cartItem }) => {
     const { imageUrl, name, price, quantity } = cartItem;
     return (
-        <div>
-            <span>{name}</span>
-            <span>{quantity}</span>
+        <div className='cart-item-container'>
+            <img src={ imageUrl } alt={name} />
+            <div className='item-details'>
+                <span className='name'>{name}</span>
+                <span className='quantity'>{quantity} x ${price}</span>
+            </div>
         </div>
     );
 }
