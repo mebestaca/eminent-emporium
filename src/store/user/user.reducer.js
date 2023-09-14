021339d66fe879774c342f75137110ac6af326e6
Marc Edison Estaca <mebestaca@gmail.com>
2023-09-14 15:07:42 -0600

    fixed user reducer

--- src/store/user/user.reducer.js
+++ src/store/user/user.reducer.js
@@ -1,12 +1,10 @@
-export const USER_ACTION_TYPES = {
-    SET_CURRENT_USER: 'SET_CURRENT_USER',
-}
+import { USER_ACTION_TYPES } from "./user.types";
 
-const INITAIL_STATE = {
+const INITIAL_STATE = {
     currentUser: null
 }
 
-export const userReducer = (state = INITAIL_STATE, action) => {
+export const userReducer = (state = INITIAL_STATE, action) => {
     const { type, payload } = action;
 
     switch(type) {
