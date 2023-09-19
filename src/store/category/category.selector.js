592a20b1e6fed08718cd2f88c60d24de6919e448
Marc Edison Estaca <mebestaca@gmail.com>
2023-09-18 22:42:14 -0600

    installed reselect

--- src/store/category/category.selector.js
+++ src/store/category/category.selector.js
@@ -3,11 +3,3 @@ export const selectCategoryMap = (state) => state.category.category.reduce((acc,
     acc[title.toLowerCase()] = items;
     return acc;
 }, {});
-
-
-    // .reduce((acc, docSnapshot) => {
-    //     const { title, items } = docSnapshot.data();
-    //     acc[title.toLowerCase()] = items;
-    //     return acc;
-    // }, {});
-    // return categoryMap;
