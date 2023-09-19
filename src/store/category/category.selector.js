3515f2eb30621c266f98d62101caaf22a1ab54f8
Marc Edison Estaca <mebestaca@gmail.com>
2023-09-19 10:08:11 -0600

    memoized category selector using createSelector from reselect library

--- src/store/category/category.selector.js
+++ src/store/category/category.selector.js
@@ -1,5 +1,20 @@
-export const selectCategoryMap = (state) => state.category.category.reduce((acc, category) => {
-    const { title, items } = category;
-    acc[title.toLowerCase()] = items;
-    return acc;
-}, {});
+import { createSelector } from "reselect";
+
+const selectCategoryReducer = (state) => state.category;
+
+export const selectCategories = createSelector(
+    [selectCategoryReducer],
+    (category) => category.category
+    
+)
+
+export const selectCategoryMap = createSelector(
+    [selectCategories],
+    (category) => 
+        category.reduce((acc, category) => {
+            const { title, items } = category;
+            acc[title.toLowerCase()] = items;
+            return acc;
+        }, {})
+)
+
