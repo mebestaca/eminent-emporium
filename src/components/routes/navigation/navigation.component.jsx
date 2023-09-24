523a641f3d37e99090f565c00f09c5bca7e6fbf0
Marc Edison Estaca <mebestaca@gmail.com>
2023-09-23 18:34:29 -0600

    removed cart context and migrated to cart reducer

--- src/components/routes/navigation/navigation.component.jsx
+++ src/components/routes/navigation/navigation.component.jsx
@@ -1,18 +1,18 @@
 import { NavigationContainer, LogoContainer, NavLink, NavLinks } from './navigation.style.jsx';
 import { ReactComponent as AppLogo } from '../../../assets/crown.svg';
-import { Fragment, useContext } from "react";
+import { Fragment } from "react";
 import { Outlet } from "react-router-dom";
 import { useSelector } from 'react-redux';
 import { signOutUser } from "../../../utils/firebase/firebase.utils";
-import { CartContext } from "../../../contexts/cart.context";
 import { setCurrentUser } from '../../../store/user/user.action.js';
+import { selectCartIsOpen } from '../../../store/cart/cart.selector.js';
 import { selectCurrentUser } from '../../../store/user/user.selector.js';
 import CartIcon from "../../cart-icon/cart-icon.component";
 import CartDropdown from "../../cart-dropdown/cart-dropdown.component";
 
 const Navigation = () => {
     const currentUser = useSelector(selectCurrentUser);
-    const { isCartOpen} = useContext(CartContext);
+    const isCartOpen = useSelector(selectCartIsOpen);
 
     const signOutHandler = async () => {
         await signOutUser();
