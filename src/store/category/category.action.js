4b494267abf86d7fe5b0c47150231df58020c9ec
Marc Edison Estaca <mebestaca@gmail.com>
2023-09-25 01:18:15 -0600

    created actions compatible with redux-thunk

--- src/store/category/category.action.js
+++ src/store/category/category.action.js
@@ -1,4 +1,20 @@
 import { createAction } from "../../utils/reducer/reducer.utils";
 import { CATEGORY_ACTION_TYPE } from "./category.types";
+import { getCategoriesAndDocuments } from "../../utils/firebase/firebase.utils";
 
-export const setCategories = (categories) => createAction(CATEGORY_ACTION_TYPE.SET_CATEGORY, categories);
+export const fetchCategoriesStart = () => createAction(CATEGORY_ACTION_TYPE.FETCH_CATEGORY_START);
+
+export const fetchCategoriesSuccess = (categories) => createAction(CATEGORY_ACTION_TYPE.FETCH_CATEGORY_SUCCESS, categories);
+
+export const fetchCategoriesFailure = (error) => createAction(CATEGORY_ACTION_TYPE.FETCH_CATEGORY_FAILURE, error);
+
+export const fetchCategoriesAsync = () => async (dispatch) => {
+    dispatch(fetchCategoriesStart());
+    try{
+        const categories = await getCategoriesAndDocuments();
+        dispatch(fetchCategoriesSuccess(categories));
+    }
+    catch(error) {
+        dispatch(fetchCategoriesFailure(error));
+    }
+}
