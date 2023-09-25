ec7a1b4ae418c9393c9fed66718db7c163d96d63
Marc Edison Estaca <mebestaca@gmail.com>
2023-09-25 01:17:46 -0600

    included thunk to middleware list

--- src/store/store.js
+++ src/store/store.js
@@ -3,6 +3,7 @@ import logger from "redux-logger";
 import { rootReducer } from "./root-reducer";
 import { persistStore, persistReducer } from "redux-persist";
 import storage from "redux-persist/lib/storage";
+import thunk from "redux-thunk";
 
 const persistConfig = {
     key: 'root',
@@ -10,7 +11,7 @@ const persistConfig = {
     blacklist: ['user']
 }
 const persistedReducer = persistReducer(persistConfig, rootReducer);
-const middleWares = [ process.env.NODE_ENV !== 'production' && logger].filter(Boolean);
+const middleWares = [ process.env.NODE_ENV !== 'production' && logger, thunk].filter(Boolean);
 const composeEnhancer = (process.env.NODE_ENV !== 'production' && 
     window && 
     window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || compose;
