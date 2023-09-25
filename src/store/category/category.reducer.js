a8f3ac1058df1ba67f9fe89ea793eb127d81a20c
Marc Edison Estaca <mebestaca@gmail.com>
2023-09-24 20:59:24 -0600

    set category reducer and type for redux-thunk

--- src/store/category/category.reducer.js
+++ src/store/category/category.reducer.js
@@ -1,17 +1,31 @@
 import { CATEGORY_ACTION_TYPE } from "./category.types";
 
 const INITIAL_STATE = {
-    category: []
+    category: [],
+    isLoading: false,
+    error: null
 }
 
 export const categoryReducer = (state = INITIAL_STATE, action={}) => {
     const { type, payload } = action;
 
     switch(type) {
-        case CATEGORY_ACTION_TYPE.SET_CATEGORY:
+        case CATEGORY_ACTION_TYPE.FETCH_CATEGORY_START:
+            return {
+                ...state,
+                isLoading: true,
+            }
+        case CATEGORY_ACTION_TYPE.FETCH_CATEGORY_FAILURE:
+            return {
+                ...state,
+                isLoading: false,
+                error: payload,
+            }
+        case CATEGORY_ACTION_TYPE.FETCH_CATEGORY_SUCCESS:
             return {
                 ...state,
                 category: payload,
+                isLoading: false,
             }
         default:
             return state;
