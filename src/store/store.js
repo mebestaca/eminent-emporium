c64ba9da092f584150baea8d7b750ec9d3b58f74
Marc Edison Estaca <mebestaca@gmail.com>
2023-09-25 20:47:55 -0600

    whitelisted cart

--- src/store/store.js
+++ src/store/store.js
@@ -8,7 +8,7 @@ import thunk from "redux-thunk";
 const persistConfig = {
     key: 'root',
     storage,
-    blacklist: ['user']
+    whitelist: ['cart']
 }
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 const middleWares = [ process.env.NODE_ENV !== 'production' && logger, thunk].filter(Boolean);
