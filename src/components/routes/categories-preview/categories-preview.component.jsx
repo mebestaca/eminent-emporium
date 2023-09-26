8c10f23a02d1e5e3c83c1bd8942d961132ca8ac7
Marc Edison Estaca <mebestaca@gmail.com>
2023-09-25 20:49:02 -0600

    implemented isLoading

--- src/components/routes/categories-preview/categories-preview.component.jsx
+++ src/components/routes/categories-preview/categories-preview.component.jsx
@@ -1,14 +1,17 @@
 import { Fragment } from "react";
 import { useSelector } from "react-redux";
 import CategoryPreview from "../../category-preview/category-preview.component"; 
-import { selectCategoryMap } from "../../../store/category/category.selector";
+import { selectCategoryMap, selectCategoryIsLoading } from "../../../store/category/category.selector";
+import Spinner from "../../spinner/spinner.component";
 
 const CategoriesPreview = () => {
     const categoriesMap = useSelector(selectCategoryMap);
-    
+    const isLoading = useSelector(selectCategoryIsLoading);
     return (
         <Fragment>
             {
+                isLoading ?
+                <Spinner /> :
                 Object.keys(categoriesMap).map((title) => {
                     const products = categoriesMap[title];
                     return (
