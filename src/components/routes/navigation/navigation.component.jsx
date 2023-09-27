3cfb2760e01f9e2ebbe1e970f5c65568c52e364c
Marc Edison Estaca <mebestaca@gmail.com>
2023-09-27 17:38:24 -0600

    implemented signout saga

--- src/components/routes/navigation/navigation.component.jsx
+++ src/components/routes/navigation/navigation.component.jsx
@@ -3,20 +3,24 @@ import { ReactComponent as AppLogo } from '../../../assets/crown.svg';
 import { Fragment } from "react";
 import { Outlet } from "react-router-dom";
 import { useSelector } from 'react-redux';
-import { signOutUser } from "../../../utils/firebase/firebase.utils";
-import { setCurrentUser } from '../../../store/user/user.action.js';
+// import { signOutUser } from "../../../utils/firebase/firebase.utils";
+// import { setCurrentUser } from '../../../store/user/user.action.js';
 import { selectCartIsOpen } from '../../../store/cart/cart.selector.js';
 import { selectCurrentUser } from '../../../store/user/user.selector.js';
 import CartIcon from "../../cart-icon/cart-icon.component";
 import CartDropdown from "../../cart-dropdown/cart-dropdown.component";
+import { useDispatch } from 'react-redux';
+import { signOutStart } from '../../../store/user/user.action.js';
 
 const Navigation = () => {
+    const dispatch = useDispatch();
     const currentUser = useSelector(selectCurrentUser);
     const isCartOpen = useSelector(selectCartIsOpen);
 
     const signOutHandler = async () => {
-        await signOutUser();
-        setCurrentUser(null);
+        // await signOutUser();
+        // setCurrentUser(null);
+        dispatch(signOutStart());
     }
 
     return (
