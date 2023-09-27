8b43e04b1dc5577c1ba4994be71b13345500fbb0
Marc Edison Estaca <mebestaca@gmail.com>
2023-09-27 01:28:26 -0600

    migrated userSessionCheck to user-saga

--- src/App.js
+++ src/App.js
@@ -5,22 +5,26 @@ import Shop from "./components/routes/shop/shop.component";
 import Checkout from "./components/checkout/checkout.component";
 import { Route, Routes } from "react-router-dom";
 import { useEffect } from "react";
-import { onAuthStateChangedListener, createUserDocumentFromAuth } from "./utils/firebase/firebase.utils";
-import { setCurrentUser } from "./store/user/user.action";
+// import { onAuthStateChangedListener, createUserDocumentFromAuth } from "./utils/firebase/firebase.utils";
+// import { setCurrentUser } from "./store/user/user.action";
+// import { getCurrentUser } from "./utils/firebase/firebase.utils";
 import { useDispatch } from "react-redux";
+import { checkUserSession } from "./store/user/user.action";
 
 const App = () => {
   const dispatch  = useDispatch();
 
   useEffect(() =>{
-    const unsubscribe = onAuthStateChangedListener((user) => {
-        if (user) {
-            createUserDocumentFromAuth(user);
-        }
-        dispatch(setCurrentUser(user));
-    });
 
-    return unsubscribe;
+    // const unsubscribe = onAuthStateChangedListener((user) => {
+    //     if (user) {
+    //         createUserDocumentFromAuth(user);
+    //     }
+    //     dispatch(setCurrentUser(user));
+    // });
+
+    // return unsubscribe;
+    dispatch(checkUserSession());
   }, [dispatch]);
 
   return (
