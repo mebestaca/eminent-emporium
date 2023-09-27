78c9ffdc30cb7fbd724033a24d7d210a73c606bf
Marc Edison Estaca <mebestaca@gmail.com>
2023-09-27 17:39:38 -0600

    added reducer that handles user signout

--- src/store/user/user.reducer.js
+++ src/store/user/user.reducer.js
@@ -15,7 +15,14 @@ export const userReducer = (state = INITIAL_STATE, action) => {
                 ...state,
                 currentUser: payload
             }
-        case USER_ACTION_TYPES.SING_IN_FAILURE:
+        case USER_ACTION_TYPES.SIGN_OUT_SUCCESS:
+            return {
+                ...state,
+                currentUser: null
+            }
+        case USER_ACTION_TYPES.SIGN_UP_FAILURE:
+        case USER_ACTION_TYPES.SIGN_IN_FAILURE:
+        case USER_ACTION_TYPES.SIGN_OUT_FAILURE:
             return {
                 ...state,
                 error: payload
