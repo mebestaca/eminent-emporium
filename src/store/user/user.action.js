2f73bf83e5f8f1bd6c384483e188605e537d8c84
Marc Edison Estaca <mebestaca@gmail.com>
2023-09-27 17:39:07 -0600

    added signout and signup actions

--- src/store/user/user.action.js
+++ src/store/user/user.action.js
@@ -3,14 +3,6 @@ import { createAction } from "../../utils/reducer/reducer.utils";
 
 export const setCurrentUser = (user) => createAction(USER_ACTION_TYPES.SET_CURRENT_USER, user);
 
-/*
-    CHECK_USER_SESSION: 'user/CHECK_USER_SESSION',
-    GOOGLE_SIGN_IN_START: 'user/GOOGLE_SIGN_IN_START',
-    EMAIL_SIGN_IN_START: 'user/EMAIL_SIGN_IN_START',
-    SIGN_IN_SUCCESS: 'user/SIGN_IN_SUCCESS',
-    SING_IN_FAILURE: 'user/SIGN_IN_FAILURE',
-*/
-
 export const checkUserSession = () => createAction(USER_ACTION_TYPES.CHECK_USER_SESSION);
 
 export const googleSignInStart = () => createAction(USER_ACTION_TYPES.GOOGLE_SIGN_IN_START);
@@ -19,4 +11,22 @@ export const emailSignInStart = (email, password) => createAction(USER_ACTION_TY
 
 export const signInSuccess = (user) => createAction(USER_ACTION_TYPES.SIGN_IN_SUCCESS, user);
 
-export const signInFailure = (error) => createAction(USER_ACTION_TYPES.SING_IN_FAILURE, error);
+export const signInFailure = (error) => createAction(USER_ACTION_TYPES.SIGN_IN_FAILURE, error);
+
+export const signUpStart = (email, password, displayedName) => 
+    createAction(
+        USER_ACTION_TYPES.EMAIL_SIGN_IN_START,
+        {
+            email, password, displayedName
+        }
+    );
+
+export const signUpSuccess = (user, additionalInfo) => createAction(USER_ACTION_TYPES.SIGN_UP_SUCCESS, { user, additionalInfo });
+
+export const signUpFailure = (error) => createAction(USER_ACTION_TYPES.SIGN_UP_FAILURE, error);
+
+export const signOutStart = () => createAction(USER_ACTION_TYPES.SIGN_OUT_START);
+
+export const signOutSuccess = () => createAction(USER_ACTION_TYPES.SIGN_OUT_SUCCESS);
+
+export const signOutFailure = (error) => createAction(USER_ACTION_TYPES.SIGN_OUT_FAILURE, error);
