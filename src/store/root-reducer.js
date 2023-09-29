224e28e0fabd5aad57bd1a5f8c9a4ca6da4da233
Marc Edison Estaca <mebestaca@gmail.com>
2023-09-29 13:45:05 -0600

    trying out userReducer from redux/toolkit

--- src/store/root-reducer.js
+++ src/store/root-reducer.js
@@ -1,5 +1,5 @@
 import { combineReducers } from "redux";
-import { userReducer } from "./user/user.reducer";
+import { userReducer } from "./toolkit/user/user.reducer";
 import { categoryReducer } from "./category/category.reducer";
 import { cartReducer } from "./cart/cart.reducer";
 
