3048a40710310aa8ee19891835750c1d976a53cc
Marc Edison Estaca <mebestaca@gmail.com>
2023-09-30 02:06:36 -0600

    removed typo userSilice to userSlicer

--- src/store/toolkit/user/user.reducer.js
+++ src/store/toolkit/user/user.reducer.js
@@ -4,16 +4,16 @@ const INITIAL_STATE = {
   currentUser: null,
 };
 
-export const userSilice = createSlice({
+export const userSlice = createSlice({
     name: 'user',
     initialState: INITIAL_STATE,
     reducers: {
         setCurrentUser(state, action) {
-            state.currentUser = action.payload
+            state.currentUser = action.payload;
         }
     }
 });
 
-export const { setCurrentUser } = userSilice.actions;
+export const { setCurrentUser } = userSlice.actions;
 
-export const userReducer = userSilice.reducer;
+export const userReducer = userSlice.reducer;
