c625aa4b31995fcf33d9216d3e78465bc1fda59b
Marc Edison Estaca <mebestaca@gmail.com>
2023-09-30 21:38:01 -0600

    implemented categoryReducer

--- src/components/routes/shop/shop.component.jsx
+++ src/components/routes/shop/shop.component.jsx
@@ -1,15 +1,25 @@
 import { Route, Routes } from 'react-router-dom';
 import { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
-import { fetchCategoriesStart } from '../../../store/category/category.action';
+import { getCategoriesAndDocuments } from '../../../utils/firebase/firebase.utils';
 import Category from '../category/category.component';
 import CategoriesPreview from '../categories-preview/categories-preview.component';
+import { setCategories } from '../../../store/toolkit/category/category.reducer';
+// import { fetchCategoriesStart } from '../../../store/category/category.action';
 
 const Shop = () => {
     const dispatch = useDispatch();
 
     useEffect(()=>{
-        dispatch(fetchCategoriesStart());
+        
+        const getCategoriesMap = async () => {
+            const categoriesArray = await getCategoriesAndDocuments('categories');
+            console.log(categoriesArray);
+            dispatch(setCategories(categoriesArray));
+        };
+      
+        getCategoriesMap();
+        // dispatch(fetchCategoriesStart());
     }, [dispatch]);
 
     return (
