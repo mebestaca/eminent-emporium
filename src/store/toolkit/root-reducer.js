520f1a8d8f57f2b400d5288606cd0da5fa2ae187
Marc Edison Estaca <mebestaca@gmail.com>
2023-10-01 00:06:23 -0600

    added cart reducer to the active root-reducer

--- src/store/toolkit/root-reducer.js
+++ src/store/toolkit/root-reducer.js
@@ -1,7 +1,7 @@
 import { combineReducers } from "@reduxjs/toolkit";
 import { userReducer } from "./user/user.reducer";
 import { categoryReducer } from "./category/category.reducer"; 
-import { cartReducer } from "../cart/cart.reducer"; 
+import { cartReducer } from "./cart/cart.reducer"; 
 
 export const rootReducer = combineReducers({
     user:userReducer,
