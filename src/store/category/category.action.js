dc07c82829befe32249906d3f4041bfabb4b87cf
Marc Edison Estaca <mebestaca@gmail.com>
2023-10-04 19:54:37 -0600

    remove unused libraries

--- src/store/category/category.action.js
+++ src/store/category/category.action.js
@@ -6,14 +6,3 @@ export const fetchCategoriesStart = () => createAction(CATEGORY_ACTION_TYPE.FETC
 export const fetchCategoriesSuccess = (categories) => createAction(CATEGORY_ACTION_TYPE.FETCH_CATEGORY_SUCCESS, categories);
 
 export const fetchCategoriesFailure = (error) => createAction(CATEGORY_ACTION_TYPE.FETCH_CATEGORY_FAILURE, error);
-
-// export const fetchCategoriesAsync = () => async (dispatch) => {
-//     dispatch(fetchCategoriesStart());
-//     try{
-//         const categories = await getCategoriesAndDocuments();
-//         dispatch(fetchCategoriesSuccess(categories));
-//     }
-//     catch(error) {
-//         dispatch(fetchCategoriesFailure(error));
-//     }
-// }
