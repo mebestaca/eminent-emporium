0ce1f05e5c6d6b2c888f21d32e85813b503cf335
Marc Edison Estaca <mebestaca@gmail.com>
2023-10-10 02:10:34 -0600

    reverted store.js to thunk

--- src/store/store.js
+++ src/store/store.js
@@ -3,11 +3,8 @@ import logger from "redux-logger";
 import { rootReducer } from "./root-reducer";
 import { persistStore, persistReducer } from "redux-persist";
 import storage from "redux-persist/lib/storage";
-import createSagaMiddleware from 'redux-saga'
-import { rootSaga } from "./root-saga";
-// import thunk from "redux-thunk";
+import thunk from "redux-thunk";
 
-const sagaMiddleware = createSagaMiddleware();
 const persistConfig = {
     key: 'root',
     storage,
@@ -16,8 +13,7 @@ const persistConfig = {
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 const middleWares = [ process.env.NODE_ENV !== 'production' && 
     logger,
-    // thunk,
-    sagaMiddleware
+    thunk,
 ].filter(Boolean);
 const composeEnhancer = (process.env.NODE_ENV !== 'production' && 
     window && 
@@ -25,7 +21,5 @@ const composeEnhancer = (process.env.NODE_ENV !== 'production' &&
 const composedEnhancers = composeEnhancer(applyMiddleware(...middleWares));
 
 export const store = createStore(persistedReducer, undefined, composedEnhancers);
-
-sagaMiddleware.run(rootSaga);
 
 export const persistor = persistStore(store);
