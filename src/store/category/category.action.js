576a45bcd6733dfb568b759fc928d1da9197510f
Marc Edison Estaca <mebestaca@gmail.com>
2023-10-10 02:09:05 -0600

    reverted category.action.js to thunk

--- src/store/category/category.action.js
+++ src/store/category/category.action.js
@@ -1,5 +1,6 @@
 import { createAction } from "../../utils/reducer/reducer.utils";
 import { CATEGORY_ACTION_TYPE } from "./category.types";
+import { getCategoriesAndDocuments } from "../../utils/firebase/firebase.utils"; 
 
 export const fetchCategoriesStart = () => createAction(CATEGORY_ACTION_TYPE.FETCH_CATEGORY_START);
 
@@ -7,13 +8,13 @@ export const fetchCategoriesSuccess = (categories) => createAction(CATEGORY_ACTI
 
 export const fetchCategoriesFailure = (error) => createAction(CATEGORY_ACTION_TYPE.FETCH_CATEGORY_FAILURE, error);
 
-// export const fetchCategoriesAsync = () => async (dispatch) => {
-//     dispatch(fetchCategoriesStart());
-//     try{
-//         const categories = await getCategoriesAndDocuments();
-//         dispatch(fetchCategoriesSuccess(categories));
-//     }
-//     catch(error) {
-//         dispatch(fetchCategoriesFailure(error));
-//     }
-// }
+export const fetchCategoriesAsync = () => async (dispatch) => {
+    dispatch(fetchCategoriesStart());
+    try{
+        const categories = await getCategoriesAndDocuments();
+        dispatch(fetchCategoriesSuccess(categories));
+    }
+    catch(error) {
+        dispatch(fetchCategoriesFailure(error));
+    }
+}
