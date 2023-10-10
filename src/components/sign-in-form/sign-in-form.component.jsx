2bfd644a1d2390fa562fe1e1afc55678d6daa3e0
Marc Edison Estaca <mebestaca@gmail.com>
2023-10-10 02:07:15 -0600

    reverted sign-in-form.component.jsx to thunk

--- src/components/sign-in-form/sign-in-form.component.jsx
+++ src/components/sign-in-form/sign-in-form.component.jsx
@@ -1,10 +1,10 @@
 import { useState } from "react";
-import { signInEmailAndPassword, signInWithGooglePopUp} from "../../utils/firebase/firebase.utils";
+import { signInWithGooglePopUp } from "../../utils/firebase/firebase.utils";
 import FormInput from "../form-input/form-input.component";
 import Button, { BUTTON_TYPE_CLASSES } from "../button/button.component";
 import { SignInContainer, ButtonsContainer } from "./sign-in-form.style";
-// import { useDispatch } from "react-redux";
-// import { googleSignInStart, emailSignInStart } from "../../store/user/user.action";
+import { useDispatch } from "react-redux";
+import { emailSignInStart } from "../../store/user/user.action";
 
 const defaultFormFields = {
     email: '',
@@ -12,7 +12,7 @@ const defaultFormFields = {
 }
 
 const SignInForm = () => {
-    // const dispatch = useDispatch();
+    const dispatch = useDispatch();
     const [formFields, setFormFields] = useState(defaultFormFields);
     const { email, password } = formFields;
 
@@ -20,8 +20,7 @@ const SignInForm = () => {
         event.preventDefault();
 
         try {
-            // dispatch(emailSignInStart(email, password));
-            await signInEmailAndPassword(email, password);
+            dispatch(emailSignInStart(email, password));
             resetFormFields();
         }
         catch(error) {
@@ -35,7 +34,6 @@ const SignInForm = () => {
 
     const logGoogleUser = async () => {
         await signInWithGooglePopUp();
-        // dispatch(googleSignInStart());
     }
 
     const resetFormFields = () => {
