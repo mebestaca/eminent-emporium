c404a59d037881887000749fa2ea82c838379b14
Marc Edison Estaca <mebestaca@gmail.com>
2023-10-11 02:55:48 -0600

    modified cateogory.reducer.js to running condition

--- src/store/category/category.reducer.js
+++ src/store/category/category.reducer.js
@@ -1,33 +1,20 @@
-import { CATEGORY_ACTION_TYPE } from "./category.types";
+import { createSlice } from "@reduxjs/toolkit";
 
-const INITIAL_STATE = {
-    category: [],
-    isLoading: false,
-    error: null
-}
+const CATEGORIES_INITIAL_STATE = {
+  category: [],
+};
 
-export const categoryReducer = (state = INITIAL_STATE, action={}) => {
-    const { type, payload } = action;
-
-    switch(type) {
-        case CATEGORY_ACTION_TYPE.FETCH_CATEGORY_START:
-            return {
-                ...state,
-                isLoading: true,
-            }
-        case CATEGORY_ACTION_TYPE.FETCH_CATEGORY_FAILURE:
-            return {
-                ...state,
-                isLoading: false,
-                error: payload,
-            }
-        case CATEGORY_ACTION_TYPE.FETCH_CATEGORY_SUCCESS:
-            return {
-                ...state,
-                category: payload,
-                isLoading: false,
-            }
-        default:
-            return state;
+export const categorySlice = createSlice({
+    name: 'category',
+    initialState: CATEGORIES_INITIAL_STATE,
+    reducers: {
+        setCategories(state, action) {
+            state.category = action.payload;
+        }
     }
-}
+
+});
+
+export const { setCategories } = categorySlice.actions;
+
+export const categoryReducer = categorySlice.reducer;
