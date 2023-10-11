89bf79a787662fa822e57f56bf491d9bdefe8246
Marc Edison Estaca <mebestaca@gmail.com>
2023-10-11 02:57:28 -0600

    modified store.js to running condition

--- src/store/store.js
+++ src/store/store.js
@@ -1,29 +1,10 @@
-import { compose, legacy_createStore as createStore, applyMiddleware } from "redux";
+import { configureStore } from "@reduxjs/toolkit";
 import logger from "redux-logger";
 import { rootReducer } from "./root-reducer";
-import { persistStore, persistReducer } from "redux-persist";
-import storage from "redux-persist/lib/storage";
-import createSagaMiddleware from 'redux-saga'
-import { rootSaga } from "./root-saga";
 
-const sagaMiddleware = createSagaMiddleware();
-const persistConfig = {
-    key: 'root',
-    storage,
-    whitelist: ['cart']
-}
-const persistedReducer = persistReducer(persistConfig, rootReducer);
-const middleWares = [ process.env.NODE_ENV !== 'production' && 
-    logger,
-    sagaMiddleware
-].filter(Boolean);
-const composeEnhancer = (process.env.NODE_ENV !== 'production' && 
-    window && 
-    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || compose;
-const composedEnhancers = composeEnhancer(applyMiddleware(...middleWares));
+const middleWares = [process.env.NODE_ENV === 'development' && logger].filter(Boolean);
 
-export const store = createStore(persistedReducer, undefined, composedEnhancers);
-
-sagaMiddleware.run(rootSaga);
-
-export const persistor = persistStore(store);
+export const store = configureStore({
+    reducer:rootReducer,
+    middleware: (defaultMiddlewares) => defaultMiddlewares().concat(middleWares)
+})
