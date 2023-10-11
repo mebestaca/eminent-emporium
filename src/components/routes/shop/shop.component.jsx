ea5a2779dec5814e209d8520bb6fd7c8d10ff181
Marc Edison Estaca <mebestaca@gmail.com>
2023-10-11 02:36:27 -0600

    modiefied shop.component.jsx to running condition

--- src/components/routes/shop/shop.component.jsx
+++ src/components/routes/shop/shop.component.jsx
@@ -1,23 +1,30 @@
-import { Route, Routes } from 'react-router-dom';
 import { useEffect } from 'react';
+import { Routes, Route } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
-import Category from '../category/category.component';
+
 import CategoriesPreview from '../categories-preview/categories-preview.component';
-import { fetchCategoriesStart } from '../../../store/category/category.action';
+import Category from '../category/category.component';
+import { getCategoriesAndDocuments } from '../../../utils/firebase/firebase.utils';
+import { setCategories } from '../../../store/category/category.action';
 
 const Shop = () => {
-    const dispatch = useDispatch();
+  const dispatch = useDispatch();
 
-    useEffect(()=>{
-        dispatch(fetchCategoriesStart());
-    }, [dispatch]);
+  useEffect(() => {
+    const getCategoriesMap = async () => {
+      const categoriesArray = await getCategoriesAndDocuments('categories');
+      dispatch(setCategories(categoriesArray));
+    };
 
-    return (
-        <Routes>
-            <Route index element={ <CategoriesPreview/> } />
-            <Route path=":category" element={ <Category/> } />
-        </Routes>
-    );   
-}
+    getCategoriesMap();
+  }, []);
+
+  return (
+    <Routes>
+      <Route index element={<CategoriesPreview />} />
+      <Route path=':category' element={<Category />} />
+    </Routes>
+  );
+};
 
 export default Shop;
