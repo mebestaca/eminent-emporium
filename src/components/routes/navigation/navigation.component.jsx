0038a0ed52d7c3606b04f5349356dca3ae5671d1
Marc Edison Estaca <mebestaca@gmail.com>
2023-10-11 02:35:40 -0600

    modief navigation.component.jsx to running condition

--- src/components/routes/navigation/navigation.component.jsx
+++ src/components/routes/navigation/navigation.component.jsx
@@ -1,51 +1,50 @@
-import { NavigationContainer, LogoContainer, NavLink, NavLinks } from './navigation.style.jsx';
-import { ReactComponent as AppLogo } from '../../../assets/crown.svg';
-import { Fragment } from "react";
-import { Outlet } from "react-router-dom";
+import { Fragment } from 'react';
+import { Outlet } from 'react-router-dom';
 import { useSelector } from 'react-redux';
-import CartIcon from "../../cart-icon/cart-icon.component";
-import CartDropdown from "../../cart-dropdown/cart-dropdown.component";
-import { selectCartIsOpen } from '../../../store/cart/cart.selector.js';
-import { selectCurrentUser } from '../../../store/user/user.selector.js';
-import { useDispatch } from 'react-redux';
-import { signOutStart } from '../../../store/user/user.action.js';
+
+import CartIcon from '../../../components/cart-icon/cart-icon.component';
+import CartDropdown from '../../../components/cart-dropdown/cart-dropdown.component';
+
+import { selectCurrentUser } from '../../../store/user/user.selector';
+import { selectIsCartOpen } from '../../../store/cart/cart.selector';
+
+import { ReactComponent as CrwnLogo } from '../../../assets/crown.svg';
+import { signOutUser } from '../../../utils/firebase/firebase.utils';
+
+import {
+  NavigationContainer,
+  NavLinks,
+  NavLink,
+  LogoContainer,
+} from './navigation.style';
 
 const Navigation = () => {
-    const dispatch = useDispatch();
-    const currentUser = useSelector(selectCurrentUser);
-    const isCartOpen = useSelector(selectCartIsOpen);
-    
-    const signOutHandler = async () => {
-        dispatch(signOutStart());
-    }
+  const currentUser = useSelector(selectCurrentUser);
+  const isCartOpen = useSelector(selectIsCartOpen);
 
-    return (
-        <Fragment>
-            <NavigationContainer>
-                <LogoContainer to='/'>
-                    <AppLogo />
-                </LogoContainer>
-                <NavLinks>
-                    <NavLink to='shop'>
-                        Shop
-                    </NavLink>
-                    {
-                        currentUser ? 
-                        (<NavLink as='span' onClick={ signOutHandler }>
-                            Sign Out
-                        </NavLink>) 
-                        : 
-                        (<NavLink to='authentication'>
-                            Sign In
-                        </NavLink>)
-                    }
-                    <CartIcon/>
-                </NavLinks>
-                { isCartOpen && <CartDropdown/> }
-            </NavigationContainer>
-            <Outlet/>
-        </Fragment>
-    );
-}
+  return (
+    <Fragment>
+      <NavigationContainer>
+        <LogoContainer to='/'>
+          <CrwnLogo className='logo' />
+        </LogoContainer>
+        <NavLinks>
+          <NavLink to='/shop'>SHOP</NavLink>
+
+          {currentUser ? (
+            <NavLink as='span' onClick={signOutUser}>
+              SIGN OUT
+            </NavLink>
+          ) : (
+            <NavLink to='/authentication'>SIGN IN</NavLink>
+          )}
+          <CartIcon />
+        </NavLinks>
+        {isCartOpen && <CartDropdown />}
+      </NavigationContainer>
+      <Outlet />
+    </Fragment>
+  );
+};
 
 export default Navigation;
