e6092e30b40d3d8401bac54ea5dd6acecc101fc9
Marc Edison Estaca <mebestaca@gmail.com>
2023-10-11 02:55:29 -0600

    modified category.action.js to running condition

--- src/store/category/category.action.js
+++ src/store/category/category.action.js
@@ -1,8 +1,5 @@
-import { createAction } from "../../utils/reducer/reducer.utils";
-import { CATEGORY_ACTION_TYPE } from "./category.types";
+import { CATEGORIES_ACTION_TYPES } from './category.types';
+import { createAction } from '../../utils/reducer/reducer.utils';
 
-export const fetchCategoriesStart = () => createAction(CATEGORY_ACTION_TYPE.FETCH_CATEGORY_START);
-
-export const fetchCategoriesSuccess = (categories) => createAction(CATEGORY_ACTION_TYPE.FETCH_CATEGORY_SUCCESS, categories);
-
-export const fetchCategoriesFailure = (error) => createAction(CATEGORY_ACTION_TYPE.FETCH_CATEGORY_FAILURE, error);
+export const setCategories = (categoriesArray) =>
+  createAction(CATEGORIES_ACTION_TYPES.SET_CATEGORIES, categoriesArray);
