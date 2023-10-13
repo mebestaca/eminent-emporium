9e726e4fbad8f7b0b97efd012d4e6f7e729fcf92
Marc Edison Estaca <mebestaca@gmail.com>
2023-10-13 01:42:02 -0600

    added <Elements> tag to enable stripe

--- src/index.js
+++ src/index.js
@@ -3,23 +3,27 @@ import ReactDOM from 'react-dom/client';
 import './index.scss';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-// import { PersistGate } from 'redux-persist/integration/react';
+import { PersistGate } from 'redux-persist/integration/react';
 import { 
   store, 
-  // persistor 
+  persistor 
 } from './store/store';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
+import { Elements } from '@stripe/react-stripe-js'; 
+import { stripePromise } from './utils/stripe/stripe.utils';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      {/* <PersistGate persistor={ persistor }> */}
+      <PersistGate loading={null} persistor={ persistor }>
         <BrowserRouter>
-          <App />
+          <Elements stripe={ stripePromise }>
+            <App />
+          </Elements>
         </BrowserRouter>
-      {/* </PersistGate> */}
+      </PersistGate>
     </Provider>
   </React.StrictMode>
 );
