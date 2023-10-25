573fd84c5ceec494562b38c8e4f78aec46b13a7a
Marc Edison Estaca <mebestaca@gmail.com>
2023-10-25 13:57:28 -0600

    added payment intent

---
+++ netlify/functions/create-payment-intent.js
@@ -0,0 +1,30 @@
+require("dotenv").config();
+
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+
+exports.handler = async (event) => {
+    console.log('stripe', stripe);
+    try{
+        const { amount } = JSON.parse(event.body);
+
+        const paymentIntent = await stripe.paymentIntent.create({
+            amount,
+            currency: "usd",
+            payment_method_types: ["card"],
+            // automatic_payment_methods: {enabled: true, allow_redirects: "never"},
+        });
+
+        return {
+            statusCode: 200,
+            body: JSON.stringify({ paymentIntent })
+        }
+    }
+    catch(error) {
+        console.log({ error });
+
+        return {
+            status: 400,
+            body: JSON.stringify({ error })
+        }
+    }
+}
