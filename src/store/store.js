af032d3c5f6e45e0c7797b3a5818d32ecf0a791d
Marc Edison Estaca <mebestaca@gmail.com>
2023-10-25 13:56:39 -0600

    messing with middlewares

--- src/store/store.js
+++ src/store/store.js
@@ -1,19 +1,19 @@
 import { configureStore } from "@reduxjs/toolkit";
-// import logger from "redux-logger";
+import logger from "redux-logger";
 import { rootReducer } from "./root-reducer";
-// import storage from "redux-persist/lib/storage";
-// import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+import { persistStore, persistReducer } from "redux-persist";
 
-// const middleWares = [process.env.NODE_ENV === 'development' && logger].filter(Boolean);
-const middleWares = [];
-// const persistConfig = {
-//     key: 'root',
-//     storage,
-//     whitelist: ['cart']
-// }
-// const persistedReducer = persistReducer(persistConfig, rootReducer);
+const middleWares = [process.env.NODE_ENV === 'development' && logger].filter(Boolean);
+
+const persistConfig = {
+    key: 'root',
+    storage,
+    whitelist: ['cart']
+}
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 export const store = configureStore({
-    reducer:rootReducer,
+    reducer:persistedReducer,
     middleware: (defaultMiddlewares) => defaultMiddlewares().concat(middleWares)
 });
-// export const persistor = persistStore(store);
+export const persistor = persistStore(store);
