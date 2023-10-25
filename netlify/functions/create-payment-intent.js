dd31c89d54931a2ea012b5fd83d3ece8e7fdd32e
Marc Edison Estaca <mebestaca@gmail.com>
2023-10-25 16:25:54 -0600

    logging

--- netlify/functions/create-payment-intent.js
+++ netlify/functions/create-payment-intent.js
@@ -3,6 +3,7 @@ require("dotenv").config();
 const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
 exports.handler = async (event, context) => {
+    console.log("serverless start");
     try{
         const { amount } = JSON.parse(event.body);
 
