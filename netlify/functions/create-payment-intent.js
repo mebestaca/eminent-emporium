329a92c40eefbf271140469c4487447df54d618e
Marc Edison Estaca <mebestaca@gmail.com>
2023-10-25 15:58:26 -0600

    added context as part of the parameters

--- netlify/functions/create-payment-intent.js
+++ netlify/functions/create-payment-intent.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 
 const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
-exports.handler = async (event) => {
+exports.handler = async (event, context) => {
     console.log("serverless function start");
     console.log("event", event);
     try{
